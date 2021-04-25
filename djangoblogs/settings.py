19923524a45b054c50587dbda5ecae9c6c99e015
Deepanshuj2610 <50258394+Deepanshuj2610@users.noreply.github.com>
2021-04-25 13:33:27 +0530

    Update settings.py

--- djangoblogs/settings.py
+++ djangoblogs/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'djangoblogs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'djangoblogs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
